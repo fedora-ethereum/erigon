fedora-ethereum/erigon

commit c45763f548f61d14e98783120ffa9bd5dd008844

check-commitment-hist-at-blk: Compute block commitment correctly + refactoring of commitment readers (#19364)
This PR :
1. Uses a SplitStateReader for the `erigon snapshots
check-commitment-hist-at-blk` command, so that the state root for the
block can be calculated correctly by reading branch data as of the
beginning of the block and account and storage state data as of the end
of block. The old behavior was that both plain state (accounts+storage)
and commitment branch data were being read as of the end of the block,
so the already updated MPT branches were being read instead of them
being recomputed.
2. Removes some duplicated commitment StateReader implementations.

---------

Co-authored-by: antonis19 <antonis19@users.noreply.github.com>

rpc/jsonrpc/eth_simulation.go
@@ -846,12 +846,12 @@ func clientLimitExceededError(message string) error {
 
 func newHistoryCommitmentOnlyReader(roTx kv.TemporalTx, sd *execctx.SharedDomains, limitReadAsOfTxNum uint64) commitmentdb.StateReader {
 	// Commitment values are read from history, whereas account/storage/code values are read from latest state
-	return rpchelper.NewCommitmentSplitStateReader(commitmentdb.NewHistoryStateReader(roTx, limitReadAsOfTxNum), commitmentdb.NewLatestStateReader(roTx, sd), true)
+	return commitmentdb.NewCommitmentSplitStateReader(commitmentdb.NewHistoryStateReader(roTx, limitReadAsOfTxNum), commitmentdb.NewLatestStateReader(roTx, sd), true)
 }
 
 func newSimulateStateReader(ttx, tx kv.TemporalTx, tsd, sd *execctx.SharedDomains) commitmentdb.StateReader {
 	// Both commitment and account/storage/code values are read from latest state *but* on different SharedDomains instances
-	return rpchelper.NewCommitmentSplitStateReader(commitmentdb.NewLatestStateReader(ttx, tsd), commitmentdb.NewLatestStateReader(tx, sd), false)
+	return commitmentdb.NewCommitmentSplitStateReader(commitmentdb.NewLatestStateReader(ttx, tsd), commitmentdb.NewLatestStateReader(tx, sd), false)
 }
 
 // computeCommitmentFromStateHistory calculates the commitment root for simulated block from state history

db/integrity/commitment_integirty.go
@@ -774,18 +774,28 @@ func CheckCommitmentHistAtBlk(ctx context.Context, db kv.TemporalRoDB, br servic
 	if err != nil {
 		return err
 	}
-	sd.GetCommitmentCtx().SetHistoryStateReader(tx, toTxNum)
+	// For blockNum==0 there is no prior commitment state (GetAsOf at txNum=0
+	// falls back to latest for the commitment domain). Use commitmentAsOf=toTxNum
+	// so the trie is restored from the committed state at the end of block 0.
+	commitmentAsOf := minTxNum
+	if blockNum == 0 {
+		commitmentAsOf = toTxNum
+	}
+	// commitment branch data view: as of beginning of the block (or end for block 0)
+	// plain state data view: as of end of the block
+	splitStateReader := commitmentdb.NewSplitHistoryReader(tx, commitmentAsOf, toTxNum, true /* withHistory */)
+	sd.GetCommitmentCtx().SetStateReader(splitStateReader)
 	sd.GetCommitmentCtx().SetTrace(logger.Enabled(ctx, log.LvlTrace))
 	sd.GetCommitmentContext().SetDeferBranchUpdates(false)
 	latestTxNum, latestBlockNum, err := sd.SeekCommitment(ctx, tx) // seek commitment again with new history state reader
 	if err != nil {
 		return err
 	}
-	if latestBlockNum != blockNum {
-		return fmt.Errorf("commitment state blockNum doesn't match blockNum: %d != %d", latestBlockNum, blockNum)
+	if blockNum > 0 && latestBlockNum != blockNum-1 { // commitment domain reads branches at end of blockNum-1
+		return fmt.Errorf("commitment state blockNum doesn't match blockNum-1: %d != %d", latestBlockNum, blockNum-1)
 	}
-	if latestTxNum != maxTxNum {
-		return fmt.Errorf("commitment state txNum doesn't match maxTxNum: %d != %d", latestTxNum, maxTxNum)
+	if blockNum > 0 && latestTxNum != minTxNum-1 { // commitment state is read as of minTxNum-1
+		return fmt.Errorf("commitment state txNum doesn't match minTxNum-1: %d != %d", latestTxNum, minTxNum-1)
 	}
 	logger.Log(lvl, "commitment recalc info", "blockNum", blockNum, "minTxNum", minTxNum, "maxTxNum", maxTxNum, "toTxNum", toTxNum)
 	trace := logger.Enabled(ctx, log.LvlTrace)

execution/commitment/backtester/backtester.go
@@ -40,6 +40,7 @@ import (
 	"github.com/erigontech/erigon/db/state/execctx"
 	"github.com/erigontech/erigon/db/state/statecfg"
 	"github.com/erigontech/erigon/execution/commitment"
+	"github.com/erigontech/erigon/execution/commitment/commitmentdb"
 )
 
 type Opt func(bt *Backtester)
@@ -206,7 +207,10 @@ func (bt Backtester) backtestBlock(ctx context.Context, tx kv.TemporalTx, block 
 	if bt.paraTrie {
 		sd.EnableParaTrieDB(bt.db)
 	}
-	sd.GetCommitmentCtx().SetStateReader(newBacktestStateReader(tx, fromTxNum, toTxNum))
+	// A history reader that reads:
+	//   - commitment data as-of the beginning of the block
+	//   - account/storage/code data as-of the end of the block
+	sd.GetCommitmentCtx().SetStateReader(commitmentdb.NewSplitHistoryReader(tx, fromTxNum, toTxNum /* withHistory */, false))
 	sd.GetCommitmentCtx().SetTrace(bt.logger.Enabled(ctx, log.LvlTrace))
 	sd.GetCommitmentCtx().EnableCsvMetrics(deriveBlockMetricsFilePrefix(blockOutputDir))
 	latestTxNum, latestBlockNum, err := sd.SeekCommitment(ctx, tx)

execution/commitment/backtester/state_reader.go
@@ -1,63 +1,0 @@
-// Copyright 2025 The Erigon Authors
-// This file is part of Erigon.
-//
-// Erigon is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Erigon is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with Erigon. If not, see <http://www.gnu.org/licenses/>.
-
-package backtester
-
-import (
-	"github.com/erigontech/erigon/db/kv"
-	"github.com/erigontech/erigon/execution/commitment/commitmentdb"
-)
-
-var _ commitmentdb.StateReader = (*backtestStateReader)(nil)
-
-// backtestStateReader implements StateReader for commitment backtesting. What we need for that is to:
-//   - read commitment data as-of the beginning of the block
-//   - read account/storage/code data as-of the end of the block
-type backtestStateReader struct {
-	commitmentReader commitmentdb.StateReader
-	plainStateReader commitmentdb.StateReader
-	commitmentAsOf   uint64
-	plainStateAsOf   uint64
-}
-
-func newBacktestStateReader(tx kv.TemporalTx, commitmentAsOf uint64, plainStateAsOf uint64) backtestStateReader {
-	return backtestStateReader{
-		commitmentReader: commitmentdb.NewHistoryStateReader(tx, commitmentAsOf),
-		plainStateReader: commitmentdb.NewHistoryStateReader(tx, plainStateAsOf),
-		commitmentAsOf:   commitmentAsOf,
-		plainStateAsOf:   plainStateAsOf,
-	}
-}
-
-func (b backtestStateReader) WithHistory() bool {
-	// we lie it is without history so we can exercise SharedDomain's in-memory DomainPut(kv.CommitmmentDomain)
-	return false
-}
-
-func (b backtestStateReader) CheckDataAvailable(_ kv.Domain, _ kv.Step) error {
-	return nil
-}
-
-func (b backtestStateReader) Read(d kv.Domain, plainKey []byte, stepSize uint64) ([]byte, kv.Step, error) {
-	if d == kv.CommitmentDomain {
-		return b.commitmentReader.Read(d, plainKey, stepSize)
-	}
-	return b.plainStateReader.Read(d, plainKey, stepSize)
-}
-
-func (b backtestStateReader) Clone(tx kv.TemporalTx) commitmentdb.StateReader {
-	return newBacktestStateReader(tx, b.commitmentAsOf, b.plainStateAsOf)
-}

execution/commitment/commitmentdb/commitment_context.go
@@ -42,133 +42,6 @@ type sd interface {
 	CommitmentCapture() bool
 }
 
-type StateReader interface {
-	WithHistory() bool
-	CheckDataAvailable(d kv.Domain, step kv.Step) error
-	Read(d kv.Domain, plainKey []byte, stepSize uint64) (enc []byte, step kv.Step, err error)
-	Clone(tx kv.TemporalTx) StateReader
-}
-
-type LatestStateReader struct {
-	getter kv.TemporalGetter
-	sd     sd
-}
-
-func NewLatestStateReader(tx kv.TemporalTx, sd sd) *LatestStateReader {
-	return &LatestStateReader{getter: sd.AsGetter(tx), sd: sd}
-}
-
-func (r *LatestStateReader) WithHistory() bool {
-	return false
-}
-
-func (r *LatestStateReader) CheckDataAvailable(d kv.Domain, step kv.Step) error {
-	// we're processing the latest state - in which case it needs to be writable
-	if frozenSteps := r.getter.StepsInFiles(d); step < frozenSteps {
-		return fmt.Errorf("%q state out of date: step %d, expected step %d", d, step, frozenSteps)
-	}
-	return nil
-}
-
-func (r *LatestStateReader) Read(d kv.Domain, plainKey []byte, stepSize uint64) (enc []byte, step kv.Step, err error) {
-	enc, step, err = r.getter.GetLatest(d, plainKey)
-	if err != nil {
-		return nil, 0, fmt.Errorf("LatestStateReader(GetLatest) %q: %w", d, err)
-	}
-	return enc, step, nil
-}
-
-func (r *LatestStateReader) Clone(tx kv.TemporalTx) StateReader {
-	return NewLatestStateReader(tx, r.sd)
-}
-
-// HistoryStateReader reads *full* historical state at specified txNum.
-// `limitReadAsOfTxNum` here is used as timestamp for usual GetAsOf.
-type HistoryStateReader struct {
-	roTx               kv.TemporalTx
-	limitReadAsOfTxNum uint64
-}
-
-func NewHistoryStateReader(roTx kv.TemporalTx, limitReadAsOfTxNum uint64) *HistoryStateReader {
-	return &HistoryStateReader{
-		roTx:               roTx,
-		limitReadAsOfTxNum: limitReadAsOfTxNum,
-	}
-}
-
-func (r *HistoryStateReader) WithHistory() bool {
-	return true
-}
-
-func (r *HistoryStateReader) CheckDataAvailable(kv.Domain, kv.Step) error {
-	return nil
-}
-
-func (r *HistoryStateReader) Read(d kv.Domain, plainKey []byte, stepSize uint64) (enc []byte, step kv.Step, err error) {
-	enc, _, err = r.roTx.GetAsOf(d, plainKey, r.limitReadAsOfTxNum)
-	if err != nil {
-		return enc, 0, fmt.Errorf("HistoryStateReader(GetAsOf) %q: (limitTxNum=%d): %w", d, r.limitReadAsOfTxNum, err)
-	}
-	return enc, kv.Step(r.limitReadAsOfTxNum / stepSize), nil
-}
-
-func (r *HistoryStateReader) Clone(tx kv.TemporalTx) StateReader {
-	return NewHistoryStateReader(tx, r.limitReadAsOfTxNum)
-}
-
-// LimitedHistoryStateReader reads from *limited* (i.e. *without-recent-files*) state at specified txNum, otherwise from *latest*.
-// `limitReadAsOfTxNum` here is used for unusual operation: "hide recent .kv files and read the latest state from files".
-type LimitedHistoryStateReader struct {
-	HistoryStateReader
-	getter kv.TemporalGetter
-	sd     sd
-}
-
-func NewLimitedHistoryStateReader(roTx kv.TemporalTx, sd sd, limitReadAsOfTxNum uint64) *LimitedHistoryStateReader {
-	return &LimitedHistoryStateReader{
-		HistoryStateReader: HistoryStateReader{
-			roTx:               roTx,
-			limitReadAsOfTxNum: limitReadAsOfTxNum,
-		},
-		getter: sd.AsGetter(roTx),
-		sd:     sd,
-	}
-}
-
-func (r *LimitedHistoryStateReader) WithHistory() bool {
-	return false
-}
-
-// Reason why we have `kv.TemporalDebugTx.GetLatestFromFiles' call here: `state.RebuildCommitmentFiles` can build commitment.kv from account.kv.
-// Example: we have account.0-16.kv and account.16-18.kv, let's generate commitment.0-16.kv => it means we need to make account.16-18.kv invisible
-// and then read "latest state" like there is no account.16-18.kv
-func (r *LimitedHistoryStateReader) Read(d kv.Domain, plainKey []byte, stepSize uint64) (enc []byte, step kv.Step, err error) {
-	var ok bool
-	var endTxNum uint64
-	// reading from domain files this way will dereference domain key correctly,
-	// GetAsOf itself does not dereference keys in commitment domain values
-	enc, ok, _, endTxNum, err = r.roTx.Debug().GetLatestFromFiles(d, plainKey, r.limitReadAsOfTxNum)
-	if err != nil {
-		return nil, 0, fmt.Errorf("LimitedHistoryStateReader(GetLatestFromFiles) %q: (limitTxNum=%d): %w", d, r.limitReadAsOfTxNum, err)
-	}
-	if !ok {
-		enc = nil
-	} else {
-		step = kv.Step(endTxNum / stepSize)
-	}
-	if enc == nil {
-		enc, step, err = r.getter.GetLatest(d, plainKey)
-		if err != nil {
-			return nil, 0, fmt.Errorf("LimitedHistoryStateReader(GetLatest) %q: %w", d, err)
-		}
-	}
-	return enc, step, nil
-}
-
-func (r *LimitedHistoryStateReader) Clone(tx kv.TemporalTx) StateReader {
-	return NewLimitedHistoryStateReader(tx, r.sd, r.limitReadAsOfTxNum)
-}
-
 type SharedDomainsCommitmentContext struct {
 	sharedDomains sd
 	updates       *commitment.Updates
@@ -439,7 +312,7 @@ func (sdc *SharedDomainsCommitmentContext) ComputeCommitment(ctx context.Context
 	sdc.justRestored.Store(false)
 
 	if saveState {
-		if err = sdc.encodeAndStoreCommitmentState(trieContext, blockNum, txNum, rootHash); err != nil {
+		if err = sdc.encodeAndStoreCommitmentState(trieContext, blockNum, txNum); err != nil {
 			return nil, err
 		}
 	}
@@ -597,7 +470,7 @@ func (sdc *SharedDomainsCommitmentContext) SeekCommitment(ctx context.Context, t
 }
 
 // encodes current trie state and saves it in SharedDomains
-func (sdc *SharedDomainsCommitmentContext) encodeAndStoreCommitmentState(trieContext *TrieContext, blockNum, txNum uint64, rootHash []byte) error {
+func (sdc *SharedDomainsCommitmentContext) encodeAndStoreCommitmentState(trieContext *TrieContext, blockNum, txNum uint64) error {
 	if trieContext == nil {
 		return errors.New("store commitment state: AggregatorContext is not initialized")
 	}

execution/commitment/commitmentdb/reader.go
@@ -1,0 +1,197 @@
+package commitmentdb
+
+import (
+	"fmt"
+
+	"github.com/erigontech/erigon/db/kv"
+)
+
+type StateReader interface {
+	WithHistory() bool
+	CheckDataAvailable(d kv.Domain, step kv.Step) error
+	Read(d kv.Domain, plainKey []byte, stepSize uint64) (enc []byte, step kv.Step, err error)
+	Clone(tx kv.TemporalTx) StateReader
+}
+
+type LatestStateReader struct {
+	sharedDomains sd
+	getter        kv.TemporalGetter
+}
+
+func NewLatestStateReader(tx kv.TemporalTx, sd sd) *LatestStateReader {
+	return &LatestStateReader{sharedDomains: sd, getter: sd.AsGetter(tx)}
+}
+
+func (r *LatestStateReader) WithHistory() bool {
+	return false
+}
+
+func (r *LatestStateReader) CheckDataAvailable(d kv.Domain, step kv.Step) error {
+	// we're processing the latest state - in which case it needs to be writable
+	if frozenSteps := r.getter.StepsInFiles(d); step < frozenSteps {
+		return fmt.Errorf("%q state out of date: step %d, expected step %d", d, step, frozenSteps)
+	}
+	return nil
+}
+
+func (r *LatestStateReader) Read(d kv.Domain, plainKey []byte, stepSize uint64) (enc []byte, step kv.Step, err error) {
+	enc, step, err = r.getter.GetLatest(d, plainKey)
+	if err != nil {
+		return nil, 0, fmt.Errorf("LatestStateReader(GetLatest) %q: %w", d, err)
+	}
+	return enc, step, nil
+}
+
+func (r *LatestStateReader) Clone(tx kv.TemporalTx) StateReader {
+	return NewLatestStateReader(tx, r.sharedDomains)
+}
+
+// HistoryStateReader reads *full* historical state at specified txNum.
+// `limitReadAsOfTxNum` here is used as timestamp for usual GetAsOf.
+type HistoryStateReader struct {
+	roTx               kv.TemporalTx
+	limitReadAsOfTxNum uint64
+}
+
+func NewHistoryStateReader(roTx kv.TemporalTx, limitReadAsOfTxNum uint64) *HistoryStateReader {
+	return &HistoryStateReader{
+		roTx:               roTx,
+		limitReadAsOfTxNum: limitReadAsOfTxNum,
+	}
+}
+
+func (r *HistoryStateReader) WithHistory() bool {
+	return true
+}
+
+func (r *HistoryStateReader) CheckDataAvailable(kv.Domain, kv.Step) error {
+	return nil
+}
+
+func (r *HistoryStateReader) Read(d kv.Domain, plainKey []byte, stepSize uint64) (enc []byte, step kv.Step, err error) {
+	enc, _, err = r.roTx.GetAsOf(d, plainKey, r.limitReadAsOfTxNum)
+	if err != nil {
+		return enc, 0, fmt.Errorf("HistoryStateReader(GetAsOf) %q: (limitTxNum=%d): %w", d, r.limitReadAsOfTxNum, err)
+	}
+	return enc, kv.Step(r.limitReadAsOfTxNum / stepSize), nil
+}
+
+func (r *HistoryStateReader) Clone(tx kv.TemporalTx) StateReader {
+	return NewHistoryStateReader(tx, r.limitReadAsOfTxNum)
+}
+
+// LimitedHistoryStateReader reads from *limited* (i.e. *without-recent-files*) state at specified txNum, otherwise from *latest*.
+// `limitReadAsOfTxNum` here is used for unusual operation: "hide recent .kv files and read the latest state from files".
+type LimitedHistoryStateReader struct {
+	HistoryStateReader
+	sharedDomains sd
+	getter        kv.TemporalGetter
+}
+
+func NewLimitedHistoryStateReader(roTx kv.TemporalTx, sd sd, limitReadAsOfTxNum uint64) *LimitedHistoryStateReader {
+	return &LimitedHistoryStateReader{
+		HistoryStateReader: HistoryStateReader{
+			roTx:               roTx,
+			limitReadAsOfTxNum: limitReadAsOfTxNum,
+		},
+		sharedDomains: sd,
+		getter:        sd.AsGetter(roTx),
+	}
+}
+
+func (r *LimitedHistoryStateReader) WithHistory() bool {
+	return false
+}
+
+// Reason why we have `kv.TemporalDebugTx.GetLatestFromFiles' call here: `state.RebuildCommitmentFiles` can build commitment.kv from account.kv.
+// Example: we have account.0-16.kv and account.16-18.kv, let's generate commitment.0-16.kv => it means we need to make account.16-18.kv invisible
+// and then read "latest state" like there is no account.16-18.kv
+func (r *LimitedHistoryStateReader) Read(d kv.Domain, plainKey []byte, stepSize uint64) (enc []byte, step kv.Step, err error) {
+	var ok bool
+	var endTxNum uint64
+	// reading from domain files this way will dereference domain key correctly,
+	// GetAsOf itself does not dereference keys in commitment domain values
+	enc, ok, _, endTxNum, err = r.roTx.Debug().GetLatestFromFiles(d, plainKey, r.limitReadAsOfTxNum)
+	if err != nil {
+		return nil, 0, fmt.Errorf("LimitedHistoryStateReader(GetLatestFromFiles) %q: (limitTxNum=%d): %w", d, r.limitReadAsOfTxNum, err)
+	}
+	if !ok {
+		enc = nil
+	} else {
+		step = kv.Step(endTxNum / stepSize)
+	}
+	if enc == nil {
+		enc, step, err = r.getter.GetLatest(d, plainKey)
+		if err != nil {
+			return nil, 0, fmt.Errorf("LimitedHistoryStateReader(GetLatest) %q: %w", d, err)
+		}
+	}
+	return enc, step, nil
+}
+
+func (r *LimitedHistoryStateReader) Clone(tx kv.TemporalTx) StateReader {
+	return NewLimitedHistoryStateReader(tx, r.sharedDomains, r.limitReadAsOfTxNum)
+}
+
+// SplitStateReader implements commitmentdb.StateReader using (potentially) different state readers for commitment
+// data and account/storage/code data.
+type SplitStateReader struct {
+	commitmentReader StateReader
+	plainStateReader StateReader
+	withHistory      bool
+}
+
+var _ StateReader = (*SplitStateReader)(nil)
+
+func (r *SplitStateReader) WithHistory() bool {
+	return r.withHistory
+}
+
+func (r *SplitStateReader) CheckDataAvailable(_ kv.Domain, _ kv.Step) error {
+	return nil
+}
+
+func (r *SplitStateReader) Read(d kv.Domain, plainKey []byte, stepSize uint64) ([]byte, kv.Step, error) {
+	if d == kv.CommitmentDomain {
+		return r.commitmentReader.Read(d, plainKey, stepSize)
+	}
+	return r.plainStateReader.Read(d, plainKey, stepSize)
+}
+
+func (r *SplitStateReader) Clone(tx kv.TemporalTx) StateReader {
+	return NewCommitmentSplitStateReader(r.commitmentReader.Clone(tx), r.plainStateReader.Clone(tx), r.withHistory)
+}
+
+func NewCommitmentSplitStateReader(commitmentReader StateReader, plainStateReader StateReader, withHistory bool) *SplitStateReader {
+	return &SplitStateReader{
+		commitmentReader: commitmentReader,
+		plainStateReader: plainStateReader,
+		withHistory:      withHistory,
+	}
+}
+
+type CommitmentReplayStateReader struct {
+	*SplitStateReader
+}
+
+func NewCommitmentReplayStateReader(ttx, tx kv.TemporalTx, tsd sd, plainStateAsOf uint64) *CommitmentReplayStateReader {
+	return &CommitmentReplayStateReader{
+		NewCommitmentSplitStateReader(NewLatestStateReader(ttx, tsd), NewHistoryStateReader(tx, plainStateAsOf), false),
+	}
+}
+
+func (crsr *CommitmentReplayStateReader) Clone(tx kv.TemporalTx) StateReader {
+	// do nothing
+	return crsr
+}
+
+// A history reader that reads:
+//   - commitment data as-of  commitmentAsOf txnum
+//   - account/storage/code data as-of plainsStateAsOf txnum
+func NewSplitHistoryReader(tx kv.TemporalTx, commitmentAsOf uint64, plainStateAsOf uint64, withHistory bool) *SplitStateReader {
+	return &SplitStateReader{
+		commitmentReader: NewHistoryStateReader(tx, commitmentAsOf),
+		plainStateReader: NewHistoryStateReader(tx, plainStateAsOf),
+		withHistory:      withHistory,
+	}
+}

rpc/jsonrpc/receipts/receipts_generator.go
@@ -24,6 +24,7 @@ import (
 	"github.com/erigontech/erigon/db/services"
 	"github.com/erigontech/erigon/db/state/execctx"
 	"github.com/erigontech/erigon/execution/chain"
+	"github.com/erigontech/erigon/execution/commitment/commitmentdb"
 	"github.com/erigontech/erigon/execution/protocol"
 	"github.com/erigontech/erigon/execution/protocol/aa"
 	"github.com/erigontech/erigon/execution/protocol/rules"
@@ -664,7 +665,7 @@ func (m *loaderMutex[K]) unlock(mu *sync.Mutex, key K) {
 
 func (g *Generator) computeCommitmentFromStateHistory(ctx context.Context, tx kv.TemporalTx, blockNum uint64, txNum uint64) ([]byte, error) {
 	receiptComputeCommitment := func(ctx context.Context, ttx kv.TemporalTx, tsd *execctx.SharedDomains) ([]byte, error) {
-		tsd.GetCommitmentCtx().SetStateReader(rpchelper.NewCommitmentReplayStateReader(ttx, tx, tsd, txNum+1))
+		tsd.GetCommitmentCtx().SetStateReader(commitmentdb.NewCommitmentReplayStateReader(ttx, tx, tsd, txNum+1))
 		minTxNum, err := g.txNumReader.Min(ctx, tx, blockNum)
 		if err != nil {
 			return nil, err

rpc/rpchelper/commitment.go
@@ -36,49 +36,6 @@ import (
 	"github.com/erigontech/erigon/execution/state/genesiswrite"
 )
 
-// splitStateReader implements commitmentdb.StateReader using (potentially) different state readers for commitment
-// data and account/storage/code data.
-type splitStateReader struct {
-	commitmentReader commitmentdb.StateReader
-	plainStateReader commitmentdb.StateReader
-	withHistory      bool
-}
-
-var _ commitmentdb.StateReader = (*splitStateReader)(nil)
-
-func (r splitStateReader) WithHistory() bool {
-	return r.withHistory
-}
-
-func (r splitStateReader) CheckDataAvailable(_ kv.Domain, _ kv.Step) error {
-	return nil
-}
-
-func (r splitStateReader) Read(d kv.Domain, plainKey []byte, stepSize uint64) ([]byte, kv.Step, error) {
-	if d == kv.CommitmentDomain {
-		return r.commitmentReader.Read(d, plainKey, stepSize)
-	}
-	return r.plainStateReader.Read(d, plainKey, stepSize)
-}
-
-func (r splitStateReader) Clone(kv.TemporalTx) commitmentdb.StateReader {
-	// Do *NOT* propagate kv.TemporalTx because each reader may need its own
-	return NewCommitmentSplitStateReader(r.commitmentReader, r.plainStateReader, r.withHistory)
-}
-
-func NewCommitmentSplitStateReader(commitmentReader commitmentdb.StateReader, plainStateReader commitmentdb.StateReader, withHistory bool) commitmentdb.StateReader {
-	return splitStateReader{
-		commitmentReader: commitmentReader,
-		plainStateReader: plainStateReader,
-		withHistory:      withHistory,
-	}
-}
-
-func NewCommitmentReplayStateReader(ttx, tx kv.TemporalTx, tsd *execctx.SharedDomains, plainStateAsOf uint64) commitmentdb.StateReader {
-	// Claim that during replay we do not operate on history, so we can temporarily save commitment state
-	return NewCommitmentSplitStateReader(commitmentdb.NewLatestStateReader(ttx, tsd), commitmentdb.NewHistoryStateReader(tx, plainStateAsOf), false)
-}
-
 type CommitmentReplay struct {
 	dirs        datadir.Dirs
 	txNumReader rawdbv3.TxNumsReader
@@ -162,7 +119,7 @@ func (r *CommitmentReplay) ComputeCustomCommitmentFromStateHistory(
 		if err != nil {
 			return nil, err
 		}
-		tsd.GetCommitmentCtx().SetStateReader(NewCommitmentReplayStateReader(ttx, tx, tsd, maxTxNum+1))
+		tsd.GetCommitmentCtx().SetStateReader(commitmentdb.NewCommitmentReplayStateReader(ttx, tx, tsd, maxTxNum+1))
 		r.logger.Debug("Touch historical keys", "fromTxNum", minTxNum, "toTxNum", maxTxNum+1)
 		_, _, err = tsd.TouchChangedKeysFromHistory(tx, minTxNum, maxTxNum+1)
 		if err != nil {

commit 624598a18f92e8167d6f56b3fde61cdc9165e8f3

fix eth simulation hive (#19866)
close #19758

rpc: fix eth_simulateV1 missing account reads falling back to empty temp
DB (this PR fixes regression introduced in PR #19364)

After this PR remains 56 failed in Hive for eth_simulateV1 for new field
MaxGasUsed

---------

Co-authored-by: Claude Sonnet 4.6 <noreply@anthropic.com>

rpc/jsonrpc/eth_simulation.go
@@ -859,8 +859,18 @@ func newHistoryCommitmentOnlyReader(roTx kv.TemporalTx, sd *execctx.SharedDomain
 }
 
 func newSimulateStateReader(ttx, tx kv.TemporalTx, tsd, sd *execctx.SharedDomains) commitmentdb.StateReader {
-	// Both commitment and account/storage/code values are read from latest state *but* on different SharedDomains instances
-	return commitmentdb.NewCommitmentSplitStateReader(commitmentdb.NewLatestStateReader(ttx, tsd), commitmentdb.NewLatestStateReader(tx, sd), false)
+	// Both commitment and account/storage/code values are read from latest state *but* on different SharedDomains instances.
+	// We use CommitmentReplayStateReader (not a plain SplitStateReader) so that Clone() only propagates the new tx to
+	// the commitment (temp DB) reader, keeping the plain state (main DB) reader pointing at the original outer-DB tx.
+	// This is critical: accounts whose data didn't change during simulation are not written to sd.mem, so when the trie
+	// reads them it must fall back to the real DB (via the original tx), not to the empty temp DB (via ttx).
+	return &commitmentdb.CommitmentReplayStateReader{
+		SplitStateReader: commitmentdb.NewCommitmentSplitStateReader(
+			commitmentdb.NewLatestStateReader(ttx, tsd),
+			commitmentdb.NewLatestStateReader(tx, sd),
+			false,
+		),
+	}
 }
 
 // computeCommitmentFromStateHistory calculates the commitment root for simulated block from state history

.github/workflows/test-hive.yml
@@ -60,7 +60,7 @@ jobs:
             max-allowed-failures: 0
           - sim: rpc
             sim-limit: compat
-            max-allowed-failures: 57 # 56 missing eth_simulateV1 MaxGasUsed in Hive tests + 1 https://github.com/erigontech/erigon/issues/19758
+            max-allowed-failures: 56 # 56 missing eth_simulateV1 MaxGasUsed in Hive tests 
     steps:
       - name: Checkout Erigon go.mod
         uses: actions/checkout@v6